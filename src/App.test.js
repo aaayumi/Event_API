571d546dd43f7525a47971a004e2303365274303
Ayumi Saito <ayumisaito27@gmail.com>
2017-11-06 16:47:55 +0100

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { expect } from 'chai';
+import { mount, render, shallow } from 'enzyme';
+import sinon from 'sinon';
+import Search from './components/Search';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
 
+describe('<Search />', () => {
+  it('renders three input components', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper.find('input')).to.have.length(3);
+  });
+  
+   it('renders `.wrappwer`', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper.find('.wrapper')).to.have.length(1);
+  });
+ });
